644edb25949b4aaa4c81f2b3dbe003afa11a35da
Manish <manishkumaryadav189@gmail.com>
2020-07-01 14:41:55 +0530

    new commit

---
+++ src/components/navbar.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+
+const NavBar = ({ totalCounters }) => {
+  // render() {
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <a className="navbar-brand" href="#">
+        NavBar{" "}
+        <span className="badge badge-pill badge-secondary">
+          {totalCounters}
+        </span>
+      </a>
+    </nav>
+  );
+  //   }
+};
+
+// class NavBar extends Component {
+//   render() {
+//     return (
+//       <nav className="navbar navbar-light bg-light">
+//         <a className="navbar-brand" href="#">
+//           NavBar{" "}
+//           <span className="badge badge-pill badge-secondary">
+//             {this.props.totalCounters}
+//           </span>
+//         </a>
+//       </nav>
+//     );
+//   }
+// }
+
+export default NavBar;
